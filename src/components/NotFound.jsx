c02d68bc1d7b2bdff4fb4b67d80b46ab7f0edc56
mdsujon428 <mdsujon000056@outlook.com>
2025-08-16 11:57:24 +0600

    redirect button added to NotFound page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <div> 
         <h1>404 - Page Not Found</h1>
+        <br />
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <br/>
+
+        <button className='go-back-btn' onClick={() => navigate('/')}>Go Back</button>
     </div>
   )
 }
