1b06cb0090217457e159ea5d51885eb69c25644a
mdsujon428 <mdsujon000056@outlook.com>
2025-08-22 09:11:52 +0600

    Api.js file added with WP CONSUMER_KEY,CONSUMER_SECRET,API_URL and function to generate OAuth signature

---
+++ src/Api.js
@@ -0,0 +1,36 @@
+ const CONSUMER_KEY="ck_fd5d8051c53561cfba3d67ca7b866658ab93099c"
+ const CONSUMER_SECRET="cs_31f61a15000723517813b8a51569aea4fd17d832"
+ const API_URL = "http://clothingbazaar.local/wp-json/wc/v3";
+
+
+
+
+ // Function to generate OAuth signature
+const generateOAuthSignature = (url, method = 'GET', params = {}) => {
+  const nonce = Math.random().toString(36).substring(2);
+  const timestamp = Math.floor(Date.now() / 1000);
+
+  const oauthParams = {
+    oauth_consumer_key: CONSUMER_KEY,
+    oauth_nonce: nonce,
+    oauth_signature_method: 'HMAC-SHA1',
+    oauth_timestamp: timestamp,
+    oauth_version: '1.0',
+  };
+
+  const allParams = { ...oauthParams, ...params };
+
+  const paramString = Object.keys(allParams)
+    .sort()
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(allParams[key])}`)
+    .join('&');
+
+  const baseUrl = url.split('?')[0]; // Ensure no query params in the base URL
+  const baseString = `${method.toUpperCase()}&${encodeURIComponent(baseUrl)}&${encodeURIComponent(paramString)}`;
+  const signingKey = `${encodeURIComponent(CONSUMER_SECRET)}&`;
+
+  const signature = CryptoJS.HmacSHA1(baseString, signingKey).toString(CryptoJS.enc.Base64);
+
+  return { ...oauthParams, oauth_signature: encodeURIComponent(signature) };
+};
+
--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import ContactForm from './components/ContactForm'
 import NotFound from './components/NotFound'
 import JobDetail,{JobDetailLoader} from './components/JobDetail'
 import Error from './components/Error'
+import './Api'
 
 
 
